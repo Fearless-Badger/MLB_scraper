4b3d0c59fad82f52269e4aeda9144774f961ecd1
Fearless-Badger <182028792+Fearless-Badger@users.noreply.github.com>
2025-02-12 23:18:53 -0600

    Created home page

--- Roster_App/backend/app.py
+++ Roster_App/backend/app.py
@@ -1,19 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
 
-@app.route("/roster/<team>")
-def disp_roster(team):
-    
-    url = f"https://www.mlb.com/{team}/roster/40-man"
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/roster", methods = ['GET', 'POST'])
+def disp_roster():
+
+    if request.method == "GET":
+        return render_template("index.html")
+
+    team = None
     try:
+        team = request.form.get("team_name")
+        url = f"https://www.mlb.com/{team}/roster/40-man"
         response = requests.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
@@ -33,7 +39,7 @@ def disp_roster(team):
             process_table(infielders, result, "Infielders")
             process_table(outfielders, result, "Outfielders")
 
-            return f"<h1>{result}</h1>"
+            return f"<p>{result}</p>"
         else:
             return "<h1>Site Not Found</h1>" # invalid path
     except Exception as e:
